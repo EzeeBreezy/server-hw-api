90faf2c3fc9b81283ed65fb0b5a4f4591716bca5
Vladyslav Panchenko <vladlibero@gmail.com>
2019-11-05 17:07:43 +0200

    api ready

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/testApi.json`)
+})
+
+app.listen(4000, '0.0.0.0', () => {
+    console.log("Express server started at http://0.0.0.0:4000")
+})
